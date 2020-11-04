62d195af94348a94a59702b46047ea93139b9df0
Ruslan Kucherov <r.kucherov@corp.mail.ru>
2020-11-04 10:43:13 +0300

    eslint

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
-import promptly from "promptly";
+import promptly from 'promptly';
 
 export default async () => {
-  console.log("Welcome to the Brain Games!");
-  const name = await promptly.prompt("May I have your name?");
+  console.log('Welcome to the Brain Games!');
+  const name = await promptly.prompt('May I have your name?');
   console.log(`Hello, ${name}!`);
 };
